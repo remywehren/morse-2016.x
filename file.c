6ecfa704a464042af689dbb1685f8ba797f26d65
Pecli <Pecli@Pecli-PC>
2016-04-21 21:29:29 +0200

    implémentation file.c

--- file.c
+++ file.c
@@ -1,18 +1,42 @@
 #include "test.h"
 
+
+
 #define FILE_TAILLE 10
 
 /** Espace de mémoire pour stoquer la file. */
 static char file[FILE_TAILLE];
-static int *in, *out;
+static int in, out;
+
+/**
+ * Indique si la file est pleine.
+ */char fileEstPleine() {
+   
+    if(((in-out)==(FILE_TAILLE-1))||((out+1) == in)){
+    }
+    return '0';
+}
+     
+ /**
+ * Indique si la file est vide.
+ */
+char fileEstVide() {
+    if(out = in){
+    return '0';
+    }else{
+        return '1';
+    }
+}
 
 /**
  * Si il y a de la place dans la file, enfile un caractère.
  * @param c Le caractère.
  */
 void fileEnfile(char c){
+   
+
     if (fileEstPleine()){
-        file[in]=c;
+        file[in] = c;
         in++;
         if (in > FILE_TAILLE){
             in = 0;
@@ -25,41 +49,28 @@ void fileEnfile(char c){
  * @return Le caractère défilé, ou 0 si la file est vide.
  */
 char fileDefile() {
+    char c;
     if(!fileEstVide()){
-        char c;
-        c=file[out];
+        c = file[out];
         out++;
-        return c;
-        if(out > FILE_TAILLE){
+        if (out > FILE_TAILLE){
             out = 0;
         }
-    }else{
-        return 0;
+        return c;
     }
-}
-/**
- * Indique si la file est vide.
- */
-char fileEstVide() {
-    if(out = in){
     return 0;
-    }
 }
-/**
- * Indique si la file est pleine.
- */
-char fileEstPleine() {
-   
-    if( ((in-out)=(FILE_TAILLE-1))||((out+1) = in)){
-    return 0;
-    }
-
 /**
  * Vide et réinitialise la file.
  */
 void fileReinitialise() {
-    // À implémenter...
+    int i;
+    for (i=0;i=(FILE_TAILLE-1);i++){
+        file[i] = 0;
+    }
 }
+
+   
 
 #ifdef TEST
 void testEnfileEtDefile() {
